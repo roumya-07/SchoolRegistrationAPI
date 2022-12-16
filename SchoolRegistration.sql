cb3a04a96f6deec0e11cc20a20339246cbb2a1f4
Roumya Ranjan Behera <95536262+roumya-07@users.noreply.github.com>
2022-12-16 21:17:12 +0530

    Update SchoolRegistration.sql

--- SchoolRegistration.sql
+++ SchoolRegistration.sql
@@ -1,12 +1,12 @@
 Create Table TblSchoolRegistration
 (
 SchoolID int Primary Key Identity(1,1),
+StateID int ,
+DistrictID int,
 SchoolName Varchar(100),
 SchoolType Varchar(50),
 SchoolLevel Varchar(50),
-SchoolPhoto Varchar(Max),
-StateID int ,
-DistrictID int
+SchoolPhoto Varchar(Max)
 );
 
 
@@ -16,12 +16,78 @@ StateID int Primary Key Identity(1,1),
 StateName Varchar(50)
 );
 
+
+
+Insert Into TblState
+Values('Odisha')
+
+Insert Into TblState
+Values('West Bengal')
+
+Insert Into TblState
+Values('Andra Pradesh')
+
+Insert Into TblState
+Values('Madhya Pradesh')
+
+Insert Into TblState
+Values('Bihar')
+
 Create Table TblDistrict
 (
 DistrictID int Primary Key Identity(1,1),
 StateID int,
 DistrictName Varchar(50)
 );
+
+
+
+Insert Into TblDistrict
+Values(1,'Cuttack')
+
+Insert Into TblDistrict
+Values(1,'Khudra')
+
+Insert Into TblDistrict
+Values(1,'Jajpur')
+
+Insert Into TblDistrict
+Values(2,'Hugil')
+
+Insert Into TblDistrict
+Values(2,'Malda')
+
+Insert Into TblDistrict
+Values(2,'Kolkata')
+
+Insert Into TblDistrict
+Values(3,'Guntur')
+
+Insert Into TblDistrict
+Values(3,'Vizag')
+
+Insert Into TblDistrict
+Values(3,'Araku')
+
+Insert Into TblDistrict
+Values(4,'Bhopal')
+
+Insert Into TblDistrict
+Values(4,'Umaria')
+
+Insert Into TblDistrict
+Values(5,'Chapra')
+
+Insert Into TblDistrict
+Values(5,'Arwal')
+
+Insert Into TblDistrict
+Values(5,'Bhojpur')
+
+Insert Into TblDistrict
+Values(5,'Nalanda')
+
+
 
 
 Create Procedure SP_SchoolRegistration
@@ -42,7 +108,7 @@ Select * From TblState
 Else if(@Action='FillDistrict')
 Select * From TblDistrict Where StateID=@StateID
 Else if(@Action='FillTable')
-Select A.*,B.StateName,C.DistrictName From TblSchoolRegistration A,TblState B, TblDistrict C Where C.StateID=B.StateID
+Select A.SchoolID,B.StateName,C.DistrictName,A.SchoolName,A.SchoolType,A.SchoolLevel,A.SchoolPhoto From TblSchoolRegistration A,TblState B, TblDistrict C Where A.StateID=B.StateID and A.DistrictID=C.DistrictID
 Else if(@Action='SelectOne')
 Select * From TblSchoolRegistration Where SchoolID=@SchoolID
 Else if(@Action='InsertOrUpdate')
@@ -56,3 +122,5 @@ End
 Else if(@Action='Delete')
 Delete From TblSchoolRegistration Where SchoolID=@SchoolID
 End
+
+
